fb4beb3ff45ebd2fcf916bb22366a1f023d5e968
Joel Carlbark <joelcb@gmail.com>
2012-05-03 22:30:45 +0200

    Tweaked the confidence_fixed function, to use scaled up ints

--- scrapeutils.py
+++ scrapeutils.py
@@ -103,5 +103,5 @@ def confidence_fixed(ups, downs):
     n = ups + downs
     z = 1.64485 #1.0 = 85%, 1.6 = 95%
     phat = float(ups) / n
-    return (phat+z*z/(2*n)-z*math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n)
+    return int(((phat+z*z/(2*n)-z*math.sqrt((phat*(1-phat)+z*z/(4*n))/n))/(1+z*z/n))*100)
 
